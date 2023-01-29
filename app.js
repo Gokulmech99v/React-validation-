adead3fd60e2b7bcf093ec178e67c65b4223bd3b
Gokulmech99v <122428879+Gokulmech99v@users.noreply.github.com>
2023-01-29 15:17:52 +0530

    Update and rename store to app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import Exc from './compon/Exc'
+
+function App() {
+  return (
+    <div className="App">
+    
+      <Exc />
+    
+    </div>
+  );
+}
+
+export default App;
+
